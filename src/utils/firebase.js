fcdab1f909f1a296693eb534bc29fc3feaa37730
Adriane <118963778+eydiiix@users.noreply.github.com>
2023-11-15 19:49:55 +0800

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { GoogleAuthProvider, deleteUser, getAuth, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getDatabase } from 'firebase/database';
@@ -14,3 +14,38 @@ export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
 export const rdb = getDatabase(app); 
+
+export const signInWithGoogle = async () => {
+
+    try {
+      const provider = new GoogleAuthProvider();
+      provider.setCustomParameters({
+        prompt: 'select_account',
+      });
+      const result = await signInWithPopup(auth, provider);
+      const email = result.user.email;
+  
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      console.log(credential);
+      console.log(result);
+  
+      return email;
+    } catch (error) {
+      throw error;
+    }
+  };
+  
+  export const deleteUserAccount = async () => {
+    try {
+      const user = auth.currentUser;
+      if (user) {
+        await deleteUser(user);
+        console.log('User account deleted successfully');
+      } else {
+        console.error('No user account to delete');
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+  
